f15719e8d1a72db0f4e232708b70c06daa31c338
Apetroaei Cezar <njroffic@gmail.com>
2023-09-08 16:26:52 +0300

    Single responsability priciples applied

--- database/database.go
+++ database/database.go
@@ -18,20 +18,21 @@ type Dbinstance struct {
 
 var DB Dbinstance
 
-func ConnectDb() {
-	dsn := fmt.Sprintf(
-		"host=db user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=Asia/Shanghai",
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-	)
+var dbEnvs = fmt.Sprintf(
+	"host=db user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=Asia/Shanghai",
+	os.Getenv("DB_USER"),
+	os.Getenv("DB_PASSWORD"),
+	os.Getenv("DB_NAME"),
+)
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+func ConnectDb() error {
+	db, err := gorm.Open(postgres.Open(dbEnvs), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
 	})
 
 	if err != nil {
 		log.Fatal("Failed to connect to database. \n", err)
+		return err
 	}
 
 	log.Println("connected")
@@ -43,4 +44,6 @@ func ConnectDb() {
 	DB = Dbinstance{
 		Db: db,
 	}
+
+	return nil
 }
