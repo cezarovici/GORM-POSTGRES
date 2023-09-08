fa59af6fc235f10430c608e8b83c0d9bff23ffe8
Apetroaei Cezar <njroffic@gmail.com>
2023-09-08 14:47:41 +0300

    setting routes in main app from routes package and connecting to a database

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,16 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/cezarovici/GORM-POSTGRES/database"
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
+	database.ConnectDb()
+
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, Cezar Dev")
-	})
+	setupRoutes(app)
 
 	app.Listen(":3000")
 }
