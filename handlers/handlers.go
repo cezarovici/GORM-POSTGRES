0df8ba7f62a8684dcaa12ef814de39be37977d24
Apetroaei Cezar <njroffic@gmail.com>
2023-09-08 14:46:50 +0300

    handlers to create and delete users

---
+++ handlers/handlers.go
@@ -0,0 +1,33 @@
+package handlers
+
+import (
+	"github.com/cezarovici/GORM-POSTGRES/database"
+	"github.com/cezarovici/GORM-POSTGRES/models"
+	"github.com/gofiber/fiber/v2"
+)
+
+func Home(c *fiber.Ctx) error {
+	return c.SendString("Hello Cezar from handlers")
+}
+
+func CreateUser(c *fiber.Ctx) error {
+	user := new(models.Users)
+
+	if err := c.BodyParser(user); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	database.DB.Db.Create(&user)
+
+	return c.Status(200).JSON(user)
+}
+
+func Users(c *fiber.Ctx) error {
+	users := []models.Users{}
+
+	database.DB.Db.Find(&users)
+
+	return c.Status(200).JSON(users)
+}
