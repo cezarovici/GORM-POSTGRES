ab796753774733f026f26a00088c5d33c4be1c62
Apetroaei Cezar <njroffic@gmail.com>
2024-02-17 00:03:24 +0200

    adding routes too

--- frontend/gorm_userauth/src/app/login/login.component.ts
+++ frontend/gorm_userauth/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../auth.service';
+
 
 @Component({
   selector: 'app-login',
@@ -11,17 +13,22 @@ import { FormGroup, FormBuilder, Validators, FormsModule, ReactiveFormsModule } 
 export class LoginComponent implements OnInit {
   form: FormGroup;
 
-  constructor(private fb: FormBuilder) {
-      this.form = this.fb.group({
-          email: ['', Validators.required],
-          password: ['', Validators.required]
-      });
-  }
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+    this.form = this.fb.group({
+        email: ['', Validators.required],
+        password: ['', Validators.required]
+    });
+}
+
 
   ngOnInit() {
   }
 
   login() {
-      console.log('Clicked the Login button');
-  }
+    const val = this.form.value;
+
+    if (val.email && val.password) {
+      this.authService.login(val.email, val.password);
+    }
+}
 }
